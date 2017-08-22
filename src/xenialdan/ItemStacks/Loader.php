fcc5ac24fd9ca9ae593e553b6297aa630cb7ab22
thebigsmileXD <thebigsmilexd@gmail.com>
2017-08-23 01:55:21 +0200

    1.2beta update

--- src/xenialdan/ItemStacks/Loader.php
+++ src/xenialdan/ItemStacks/Loader.php
@@ -5,9 +5,9 @@ namespace xenialdan\ItemStacks;
 use pocketmine\plugin\PluginBase;
 use xenialdan\ItemStacks\task\StackTask;
 
-class Loader extends PluginBase {
+class Loader extends PluginBase{
 
-	public function onEnable() {
+	public function onEnable(){
 		$this->saveResource('config.yml');
 		$this->getServer()->getScheduler()->scheduleDelayedRepeatingTask(new StackTask($this), $this->getConfig()->get('task-speed', 15), $this->getConfig()->get('task-speed', 15));
 	}
--- src/xenialdan/ItemStacks/task/StackTask.php
+++ src/xenialdan/ItemStacks/task/StackTask.php
@@ -7,26 +7,26 @@ use pocketmine\scheduler\PluginTask;
 use pocketmine\Server;
 use xenialdan\ItemStacks\Loader;
 
-class StackTask extends PluginTask {
+class StackTask extends PluginTask{
 	/** @var Loader $plugin */
 	private $plugin;
 
-	public function __construct(Loader $owner) {
+	public function __construct(Loader $owner){
 		parent::__construct($owner);
 		$this->plugin = $owner;
 	}
 
-	public function onRun($currentTick) {
-		foreach (Server::getInstance()->getLevels() as $level) {
-			foreach ($level->getEntities() as $entity) {
-				if (!$entity instanceof Item || $entity->closed) continue;
+	public function onRun(int $currentTick){
+		foreach (Server::getInstance()->getLevels() as $level){
+			foreach ($level->getEntities() as $entity){
+				if (!$entity instanceof Item || $entity->isClosed()) continue;
 				if ($entity->getItem()->getCount() >= $entity->getItem()->getMaxStackSize()) continue;
 				if (empty($entities = $level->getNearbyEntities($entity->getBoundingBox()->grow(1, 1, 1), $entity))) continue;
-				else {
-					foreach ($entities as $possibleItem) {
-						if (!$possibleItem instanceof Item || $possibleItem->closed) continue;
+				else{
+					foreach ($entities as $possibleItem){
+						if (!$possibleItem instanceof Item || $possibleItem->isClosed()) continue;
 						if ($possibleItem->getItem()->getCount() > $possibleItem->getItem()->getMaxStackSize()) continue;
-						if ($entity->getItem()->equals($possibleItem->getItem(), true, true)) {
+						if ($entity->getItem()->equals($possibleItem->getItem(), true, true)){
 							if (($newCount = $entity->getItem()->getCount() + $possibleItem->getItem()->getCount()) >= $entity->getItem()->getMaxStackSize()) continue;
 							//stack
 							$this->plugin->getLogger()->debug('Stacked ' . $entity->getItem() . ' with ' . $possibleItem->getItem());
@@ -40,7 +40,7 @@ class StackTask extends PluginTask {
 		}
 	}
 
-	public function cancel() {
+	public function cancel(){
 		$this->getHandler()->cancel();
 	}
 }
