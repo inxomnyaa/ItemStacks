8eef2c60feb1b7e55ea6e5802cbbc374176d10b7
SalmonDE <salmonde@salmonde.de>
2018-10-28 17:31:53 +0100

    Update to API 3.x.x

--- src/xenialdan/ItemStacks/task/StackTask.php
+++ src/xenialdan/ItemStacks/task/StackTask.php
@@ -1,31 +1,30 @@
 <?php
+declare(strict_types = 1);
 
 namespace xenialdan\ItemStacks\task;
 
-use pocketmine\entity\Item;
-use pocketmine\scheduler\PluginTask;
-use pocketmine\Server;
+use pocketmine\entity\object\ItemEntity;
+use pocketmine\scheduler\Task;
 use xenialdan\ItemStacks\Loader;
 
-class StackTask extends PluginTask{
+class StackTask extends Task{
 	/** @var Loader $plugin */
 	private $plugin;
 
 	public function __construct(Loader $owner){
-		parent::__construct($owner);
 		$this->plugin = $owner;
 	}
 
 	public function onRun(int $currentTick){
-		foreach (Server::getInstance()->getLevels() as $level){
+		foreach ($this->plugin->getServer()->getLevels() as $level){
 			foreach ($level->getEntities() as $entity){
-				if (!$entity instanceof Item || $entity->isClosed()) continue;
+				if (!$entity instanceof ItemEntity || $entity->isClosed()) continue;
 				if ($entity->getItem()->getCount() >= $entity->getItem()->getMaxStackSize()) continue;
-				if (empty($entities = $level->getNearbyEntities($entity->getBoundingBox()->grow(1, 1, 1), $entity))) continue;
+				if (empty($entities = $level->getNearbyEntities($entity->getBoundingBox()->expandedCopy(1, 1, 1), $entity))) continue;
 				else{
 					foreach ($entities as $possibleItem){
-						if (!$possibleItem instanceof Item || $possibleItem->isClosed()) continue;
-						if ($possibleItem->getItem()->getCount() > $possibleItem->getItem()->getMaxStackSize()) continue;
+						if (!$possibleItem instanceof ItemEntity || $possibleItem->isClosed()) continue;
+						if ($possibleItem->getItem()->getCount() >= $possibleItem->getItem()->getMaxStackSize()) continue;
 						if ($entity->getItem()->equals($possibleItem->getItem(), true, true)){
 							if (($newCount = $entity->getItem()->getCount() + $possibleItem->getItem()->getCount()) >= $entity->getItem()->getMaxStackSize()) continue;
 							//stack
@@ -44,5 +43,3 @@ class StackTask extends PluginTask{
 		$this->getHandler()->cancel();
 	}
 }
-
-?>
